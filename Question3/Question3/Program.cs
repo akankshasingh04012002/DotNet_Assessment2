a8965939fe0c2cc7ec27e6a928e55abcd7624820
akankshasingh04012002 <akanksha.singh@knoldus.com>
2024-03-04 15:37:54 +0530

    added solutions

--- Question3/Question3/Program.cs
+++ Question3/Question3/Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stopwatch stopwatch = new Stopwatch();
+
+        // Start the stopwatch
+        stopwatch.Start();
+
+        // Waiting for user input
+        Console.WriteLine("Press any key to stop the stopwatch...");
+        Console.ReadKey(true);
+
+        // Stop the stopwatch
+        stopwatch.Stop();
+
+        // Display the duration
+        TimeSpan duration = stopwatch.GetDuration();
+        Console.WriteLine($"Duration: {duration.TotalSeconds} seconds");
+    }
+}
+
+public class Stopwatch
+{
+    private DateTime startTime;
+    private DateTime stopTime;
+    private bool isRunning;
+
+    public void Start()
+    {
+        if (isRunning)
+        {
+            throw new InvalidOperationException("Stopwatch is already running.");
+        }
+
+        startTime = DateTime.Now;
+        isRunning = true;
+    }
+
+    public void Stop()
+    {
+        if (!isRunning)
+        {
+            throw new InvalidOperationException("Stopwatch is not running.");
+        }
+
+        stopTime = DateTime.Now;
+        isRunning = false;
+    }
+
+    public TimeSpan GetDuration()
+    {
+        if (!isRunning)
+        {
+            return stopTime - startTime;
+        }
+        else
+        {
+            throw new InvalidOperationException("Stopwatch is still running.");
+        }
+    }
+}
--- Question4/Question4/Program.cs
+++ Question4/Question4/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Post
+{
+    private string title;
+    private string description;
+    private DateTime created;
+    private int votes;
+
+    // Constructor to initialize the post
+    public Post(string title, string description)
+    {
+        this.title = title;
+        this.description = description;
+        created = DateTime.Now;
+        votes = 0; // Initialize votes to 0
+    }
+
+    // Method to up-vote the post
+    public void UpVote()
+    {
+        votes++;
+    }
+
+    // Method to down-vote the post
+    public void DownVote()
+    {
+        votes--;
+    }
+
+    // Method to display the current vote value
+    public void DisplayCurrentVotes()
+    {
+        Console.WriteLine($"Current votes for post '{title}': {votes}");
+    }
+
+    // Main method to test the Post class
+    public static void Main(string[] args)
+    {
+        // Create a new post
+        Post post = new Post("Title of the Post", "Description of the Post");
+
+        // Up-vote and down-vote the post a few times
+        post.UpVote();
+        post.UpVote();
+        post.DownVote();
+        post.UpVote();
+        post.DownVote();
+
+        // Display the current vote value
+        post.DisplayCurrentVotes();
+    }
+}
